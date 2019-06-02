b287562dc7b668763a1da0ea9241201a92051938
Shunsei <khan.sufyan.cs@gmail.com>
2019-06-01 22:58:18 -0400

    Added index and root routes

---
+++ app.js
@@ -0,0 +1,56 @@
+var express 	= require('express'),
+	bodyParser 	= require('body-parser'),
+	mongoose 	= require('mongoose'),
+	app 		= express();
+
+// APP CONFIG
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.connect("mongodb://localhost/news_site");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+// MONGOOSE/MODEL CONFIG
+var articleSchema = new mongoose.Schema(
+	{
+		title: String,
+		image: String,
+		content: String,
+		like: 
+		{
+			type: Number,
+			default: 0
+		},
+		dislike: 
+		{
+			type: Number,
+			default: 0
+		},
+		created: 
+		{	
+			type: Date,
+			default: Date.now
+		}
+	}
+);
+
+var Article = mongoose.model("Article", articleSchema);
+
+// RESTFUL ROUTES
+
+// Index Route
+app.get("/articles", function(req, res) {
+	res.render("index");
+});
+
+// Root Route
+app.get("/", function(req, res) {
+	res.redirect("/articles");
+});
+
+app.listen(3000, function() {
+	console.log("Server started on port 3000.");
+});
